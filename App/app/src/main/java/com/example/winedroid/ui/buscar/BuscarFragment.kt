35df698e3dc30ff8f4969dc2c1db92d9e9df7160
Boosted <christianromansanchez@gmail.com>
2021-01-11 10:34:09 +0100

    Development busqueda y ficha vino

---
+++ App/app/src/main/java/com/example/winedroid/ui/buscar/BuscarFragment.kt
@@ -0,0 +1,62 @@
+package com.example.winedroid.ui.buscar
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.winedroid.R
+import com.example.winedroid.ui.buscar.dummy.DummyContent
+
+/**
+ * A fragment representing a list of Items.
+ */
+class BuscarFragment : Fragment() {
+
+    private var columnCount = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        arguments?.let {
+            columnCount = it.getInt(ARG_COLUMN_COUNT)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_buscar_list, container, false)
+
+        // Set the adapter
+        if (view is RecyclerView) {
+            with(view) {
+                layoutManager = when {
+                    columnCount <= 1 -> LinearLayoutManager(context)
+                    else -> GridLayoutManager(context, columnCount)
+                }
+                adapter = VinoAdapter(DummyContent.ITEMS)
+            }
+        }
+        return view
+    }
+
+    companion object {
+
+        // TODO: Customize parameter argument names
+        const val ARG_COLUMN_COUNT = "column-count"
+
+        // TODO: Customize parameter initialization
+        @JvmStatic
+        fun newInstance(columnCount: Int) =
+            BuscarFragment().apply {
+                arguments = Bundle().apply {
+                    putInt(ARG_COLUMN_COUNT, columnCount)
+                }
+            }
+    }
+}
--- App/app/src/main/java/com/example/winedroid/ui/home/HomeFragment.kt
+++ App/app/src/main/java/com/example/winedroid/ui/home/HomeFragment.kt
@@ -1,14 +1,22 @@
 package com.example.winedroid.ui.home
 
+import android.Manifest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.PermissionRequest
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.winedroid.R
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.DexterError
+import com.karumi.dexter.listener.PermissionRequestErrorListener
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class HomeFragment : Fragment() {
 
@@ -26,6 +34,45 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+        pedirMultiplesPermisos()
         return root
+    }
+
+    private fun pedirMultiplesPermisos() {
+        // Indicamos el permisos y el manejador de eventos de los mismos
+        Dexter.withActivity(activity)
+            .withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    // ccomprbamos si tenemos los permisos de todos ellos
+                    if (report.areAllPermissionsGranted()) {
+                        //Toast.makeText(getContext(), "¡Todos los permisos concedidos!", Toast.LENGTH_SHORT).show();
+                    }
+
+                    // comprobamos si hay un permiso que no tenemos concedido ya sea temporal o permanentemente
+                    if (report.isAnyPermissionPermanentlyDenied()) {
+                        // abrimos un diálogo a los permisos
+                        //openSettingsDialog();
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<com.karumi.dexter.listener.PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                     token?.continuePermissionRequest()
+                }
+            }).withErrorListener(object : PermissionRequestErrorListener {
+                override fun onError(error: DexterError?) {
+
+                }
+            })
+            .onSameThread()
+            .check()
     }
 }
